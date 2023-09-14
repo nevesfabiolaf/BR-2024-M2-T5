6d30bcaac41a3276cc710ed56d078b47934ebf33
Fabíola Alves <neves.fabiolaf@gmail.com>
2023-09-14 20:04:07 -0300

    AULA 4: Menu, score e death_count

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -5,8 +5,8 @@ from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
+Y_POS_DUCK = 345
 JUMP_VEL = 8.5
-Y_POS_DUCKING = 345
 
 
 class Dinosaur(Sprite):
@@ -19,28 +19,28 @@ class Dinosaur(Sprite):
         self.jump_vel = JUMP_VEL
         self.dino_run = True
         self.dino_jump = False
-        self.dino_ducking = False
+        self.dino_duck = False
 
     def update(self, user_input):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
-        elif self.dino_ducking:
+        elif self.dino_duck:
             self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-            self.dino_ducking = False
+            self.dino_duck = False
         elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_jump = False
             self.dino_run = False
-            self.dino_ducking = True
-        elif not self.dino_jump and not self.dino_ducking:
+            self.dino_duck = True
+        elif not self.dino_jump and not self.dino_duck:
             self.dino_jump = False
             self.dino_run = True
-            self.dino_ducking = False
+            self.dino_duck = False
             
         if self.step_index >= 10:
             self.step_index = 0
@@ -68,9 +68,9 @@ class Dinosaur(Sprite):
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
-        self.dino_rect.y = Y_POS_DUCKING
+        self.dino_rect.y = Y_POS_DUCK
         self.step_index += 1
-        self.dino_ducking = False
+        self.dino_duck = False
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
